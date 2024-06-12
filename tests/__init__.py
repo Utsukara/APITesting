7e8debc129a08969aaaeb91e0c13c80a49d025ba
Utsukara <jsparker.ms@gmail.com>
2024-06-11 21:22:40 -0500

    deleted another .py

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Add the app directory to the sys.path so that the app module can be imported easily
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# You can also perform other common setup tasks here if needed
